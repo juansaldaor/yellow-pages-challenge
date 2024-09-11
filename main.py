579afb061190ddcdc2321a76fa1a4db182dbddd4
juansaldaor <juansaldaor@gmail.com>
2024-09-11 08:40:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-# Tu código aquí
+people = {
+  "Ana": "+4-651-928088",
+  "Juan": "+23-698-536640",
+  "Pedro": "+40-659-506848",
+  "Luis": "+20-615-184747",
+  "Maria": "+78-671-257932",
+  "Sofia": "+30-611-770853",
+  "Jose": "+27-669-616952",
+  "Lucia": "+38-642-779107",
+  "Carlos": "+19-665-202837",
+  "Marta": "+38-691-898782",
+  "Miguel": "+32-636-538868",
+  "Laura": "+80-670-553129",
+  "David": "+80-650-777691",
+  "Paula": "+9-645-756359",
+  "Andres": "+31-650-896836",
+  "Elena": "+65-658-231873",
+  "Jorge": "+35-638-631914",
+  "Raul": "+54-631-199248",
+  "Clara": "+94-642-663803",
+  "Sara": "+48-657-677260"
+}
+
+
+while True:
+    print("HOLA! Bienvenido al directorio telefonico de COMPUMUNDOHIPERMEGARED")
+    print("                                     ")
+    print("digita 1 si quieres ver toda la lista")
+    print("digita 2 si quieres agregar un nuevo usuario ")
+    print("digita 3 si quieres modificar un usuario existente")
+    print("digita 4 si quieres eliminar un usuario")
+
+    opcion_usuario = input(int("digitalo aqui: "))
+
+    if opcion_usuario == 1:
+        print (people{})
+
+    elif opcion_usuario == 2:
+            
+
+    elif opcion_usuario == 3:
+    
+    elif opcion_usuario == 4:
+
+    else
+        
